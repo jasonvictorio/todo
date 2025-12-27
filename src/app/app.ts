cdba488e606f15daf653d74456a053d1da838367
Jason Victorio <jason@jasonvictorio.com>
2025-12-27 15:04:30 +1300

    feat: simplify app structure by removing placeholder content and enhancing task management functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { v4 as uuid } from 'uuid'
+
+interface Task {
+  id: string
+  text: string
+  completed: boolean
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('todo');
+  task = ''
+  tasks: Task[] = []
+
+  get incompleteTasks() {
+    return this.tasks.filter((t) => t.completed)
+  }
+
+  get completeTasks() {
+    return this.tasks.filter((t) => !t.completed)
+  }
+
+  createTask(text: string) {
+    return {
+      id: uuid(),
+      text: text,
+      completed: false,
+    }
+  }
+
+  addTask() {
+    if (this.task.trim() === '') return
+    this.tasks = [this.createTask(this.task.trim()), ...this.tasks]
+    this.task = ''
+  }
+
+  toggleTask(task: Task) {
+    const toggledTask = { ...task, completed: !task.completed }
+    this.tasks = this.tasks.map((t) => (t.id !== task.id ? t : toggledTask))
+  }
 }
