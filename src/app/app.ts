4a493ab87e6015ed7ee35d9dfb54f14c921de231
Jason Victorio <jason@jasonvictorio.com>
2026-01-02 22:13:11 +1300

    feat: implement OnInit lifecycle hook for task initialization and refactor task management methods

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common'
-import { Component } from '@angular/core'
+import { Component, type OnInit } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { v4 as uuid } from 'uuid'
 import { TaskItemComponent } from './task-item/task-item'
@@ -16,10 +16,22 @@ export type Task = {
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   task = ''
   tasks: Task[] = []
   today = new Date()
+
+  ngOnInit() {
+    const tasksString = localStorage.getItem('todo')
+    if (tasksString) {
+      try {
+        const tasks = JSON.parse(tasksString)
+        this.tasks = tasks
+      } catch (e) {
+        console.error('Could not parse todos from local storage', e)
+      }
+    }
+  }
 
   get incompleteTasks() {
     return this.tasks.filter((t) => !t.completed)
@@ -39,16 +51,23 @@ export class App {
 
   addTask() {
     if (this.task.trim() === '') return
-    this.tasks = [this.createTask(this.task.trim()), ...this.tasks]
+    this.updateTasks([this.createTask(this.task.trim()), ...this.tasks])
     this.task = ''
   }
 
   toggleTask(task: Task) {
     const toggledTask = { ...task, completed: !task.completed }
-    this.tasks = this.tasks.map((t) => (t.id !== task.id ? t : toggledTask))
+    this.updateTasks(
+      this.tasks.map((t) => (t.id !== task.id ? t : toggledTask)),
+    )
   }
 
   deleteTask(task: Task) {
-    this.tasks = this.tasks.filter((t) => t.id !== task.id)
+    this.updateTasks(this.tasks.filter((t) => t.id !== task.id))
+  }
+
+  updateTasks(tasks: Task[]) {
+    this.tasks = tasks
+    localStorage.setItem('todo', JSON.stringify(tasks))
   }
 }
